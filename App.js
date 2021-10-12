fb7eb9c322f48cb6dc4867deac7967fe6a8d6a9c
balu-kapoor <brkapoor11@gmail.com>
2021-10-12 19:14:47 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  View,
+  ActivityIndicator,
+  SafeAreaView,
+  BackHandler,
+  Alert,
+} from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert("Hold on!", "Are you sure you want to go back?", [
+        {
+          text: "Cancel",
+          onPress: () => null,
+          style: "cancel",
+        },
+        { text: "YES", onPress: () => BackHandler.exitApp() },
+      ]);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, []);
+  const [isLoadong, setLoading] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <WebView
+        originWhitelist={["*"]}
+        source={{ uri: "https://palamlottery.com" }}
+        onLoadStart={(syntheticEvent) => {
+          setLoading(true);
+        }}
+        onLoadEnd={(syntheticEvent) => {
+          setLoading(false);
+        }}
+      />
+      {isLoadong && (
+        <ActivityIndicator
+          style={{
+            position: "absolute",
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#fff",
+            opacity: 0.5,
+          }}
+          color="#dd1717"
+          size="large"
+        />
+      )}
+      <StatusBar style="light" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
